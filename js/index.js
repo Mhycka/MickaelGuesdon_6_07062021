c04d960caee8e7b24ac3e3f85124360d14ccbcd9
Guesdon Mickael <guesdon.mickael@outlook.fr>
2021-06-18 14:45:01 +0200

    commit 18/06
    section photographes page d'accueuil n JS

---
+++ js/index.js
@@ -0,0 +1,35 @@
+
+let myUrl = 'Data/FisheyeData.json';
+fetch(myUrl , {method : "GET"})
+.then(data => {
+    return data.json()
+}).then(datas =>{
+
+    let dataPhotographers = datas.photographers
+    console.log(datas)
+    console.log(dataPhotographers)
+
+    let sectionPhotographers = document.getElementById('photographers');
+
+    dataPhotographers.forEach((photographe) =>{
+        // console.log(photographe.tags)
+    
+        let cardPhotographers = document.createElement('article');
+
+        cardPhotographers.className = photographe.tags.join(' ') + ' articlePh';
+
+        let templatePhotographer = `
+            <a href="photographers.html?id=${photographe.id}" title="${photographe.name}">
+                <img src="${photographe.portrait}" alt="${photographe.alt}">
+                <h2 class="name">${photographe.name}</h2>
+            </a>
+            <p class="location">${photographe.city}, ${photographe.country}</p>
+            <p class="tagline">${photographe.tagline}</p>
+            <p class="price">${photographe.price}€/jour</p>
+            <ul class="filter">${photographe.tags.map(tag =>
+                `<li data-filter="${tag}">#${tag}</li>`).join("")}</ul>`
+
+        sectionPhotographers.appendChild(cardPhotographers);
+        cardPhotographers.innerHTML = templatePhotographer;
+
+    })
