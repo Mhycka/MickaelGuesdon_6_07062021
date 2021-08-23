b75d5f781d03f84cd033ca3bdee525d2d2c06ca4
Guesdon Mickael <guesdon.mickael@outlook.fr>
2021-08-23 16:17:40 +0200

    commit 23/08
    travaille sur le previous picture on lightbox

--- js/lightbox.js
+++ js/lightbox.js
@@ -4,7 +4,7 @@ function LightboxParam() {
     let getMedias = Array.from(document.getElementsByClassName('ph-media'));
     //console.log(getMedias)
         
-    function init() {
+    function init(currentMedia) {
 
         //cliquer ppour verifier si c'est une video ou une image et creer le html  correspondant.
 
@@ -13,9 +13,9 @@ function LightboxParam() {
             let lightBoxMedia = document.getElementById('lightbox_media');
             let src = mediaWorks.childNodes[1].src;
             let nameSrc =  mediaWorks.title;
-            // console.log(mediaWorks)
+            //console.log(mediaWorks)
             let dataType = mediaWorks.childNodes[1].getAttribute('data-type');
-            // console.log(dataType , nameSrc)
+            //console.log(dataType , nameSrc)
             let elt = null ;
             if( dataType == 'image' ) {
                 // console.log('ok img')
@@ -29,26 +29,29 @@ function LightboxParam() {
             }
             
             elt.setAttribute('src', src);
-            elt.setAttribute('alt', mediaWorks.alt);
+            elt.setAttribute('alt', mediaWorks.title);
             elt.setAttribute('id' , 'works_lightbox_media');
 
             //console.log(elt)
 
             let lightBoxName = document.getElementById('works_lightbox_name');
-           
+
             this.currentIndex = index;
 
-            // console.log(this.currentIndex)
+           
+            console.log(this.currentIndex)
 
             document.getElementById('works-lightbox').style.display = 'block';
 
             lightBoxMedia.innerHTML = '';
             lightBoxMedia.append(elt);
             lightBoxName.innerHTML = nameSrc;
+            this.currentIndex= index;
+
         }))
 
-        this.previous(document.querySelector('.left-arrow-lightbox'), getMedias);
-        this.next(document.querySelector('.right-arrow-lightbox'), getMedias);
+        this.previous(document.querySelector('.left-arrow-lightbox'), getMedias, currentMedia);
+        this.next(document.querySelector('.right-arrow-lightbox'), getMedias, currentMedia);
         this.close();
         this.keyboard(getMedias);
     };
@@ -58,22 +61,34 @@ function LightboxParam() {
             this.currentIndex -= 1;
             let lightBoxMedia = document.getElementById('works_lightbox_media');
             let lightBoxName = document.getElementById('works_lightbox_name');
+            let src = media[this.currentIndex].childNodes[1].src;
+            let nameSrc = media[this.currentIndex].title;
 
-             let dataType = media[this.currentIndex].childNodes[1].getAttribute('data-type');
+            console.log(this.currentIndex)
 
-            console.log(dataType)
-            console.log( media[this.currentIndex].childNodes[1])
+            let dataType = media[this.currentIndex].childNodes[1].getAttribute('data-type');
 
-            if (this.currentIndex < 0 && dataType == 'image' ) {
+            // console.log(currentIndex)
+            // console.log(dataType)
+            // console.log( media[this.currentIndex].childNodes[1])
+            console.log(media)
+
+            if (this.currentIndex < 0  && dataType == 'image' ) {
                 this.currentIndex = media.length - 1;
                 let eltImage = document.createElement('img');
                 elt = eltImage ;
-            }
+              } //else if (dataType == 'video') {
+            //     this.currentIndex = media.length - 1;
+            //     let eltVideo = document.createElement('video');
+            //     elt = eltVideo
+            //  }
 
-            let src = media[this.currentIndex].childNodes[1].src;
-            let nameSrc = media[this.currentIndex].title;
+            // elt.setAttribute('src', src);
+            // elt.setAttribute('alt', media.title);
+            // elt.setAttribute('id' , 'works_lightbox_media');
+
+            
             // console.log(src, nameSrc)
-
             // console.log(media[this.currentIndex] , this.currentIndex )
 
             lightBoxMedia.src = src;
